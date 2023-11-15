4d3b381b2aa0937ec1478786328d4abe7212a7df
Leonid Akinin <fatzy@protonmail.com>
2023-11-15 17:02:47 -0500

    removed unused inclusion statements

--- 02_install_exec/src/setup.py
+++ 02_install_exec/src/setup.py
@@ -31,10 +31,6 @@ setup(
     name="pymalware_install_exec",
     version="1.0.0",
     packages=find_packages(),
-    include_package_data=True,
-    package_data={
-        "": ["*.exe", "*.spec"]
-    },
     cmdclass={
         "install": CustomInstall,
     },
